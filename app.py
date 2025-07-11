cef45565ed2db6e12c2b3df392d1af13999c7fda
mikolsvonez438 <mikolsvonez@gmail.com>
2025-07-11 14:04:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def api_related():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return send_file("templates/index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
